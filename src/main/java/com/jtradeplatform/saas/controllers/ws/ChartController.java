0801439751ccd3213de942d7a42e222bd5a7535b
Stanislav <denfas112@gmail.com>
2021-05-30 14:34:54 +0300

    move ws controller, docker: added influxdb, removed clickhouse

--- src/main/java/com/jtradeplatform/saas/controllers/ws/ChartController.java
+++ src/main/java/com/jtradeplatform/saas/controllers/ws/ChartController.java
@@ -1,11 +1,11 @@
-package com.jtradeplatform.saas.controllers;
+package com.jtradeplatform.saas.controllers.ws;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class WebSocketController {
+public class ChartController {
     //@MessageMapping("/hello")
     //@SendTo("/topic/greetings")
     //public Greeting greeting(HelloMessage message) throws Exception {
---
+++ src/main/java/com/jtradeplatform/saas/controllers/ws/EventController.java
@@ -0,0 +1,13 @@
+package com.jtradeplatform.saas.controllers.ws;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EventController {
+    //@MessageMapping("/hello")
+    //@SendTo("/topic/greetings")
+    //public Greeting greeting(HelloMessage message) throws Exception {
+    //    Thread.sleep(1000); // simulated delay
+    //    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    //}
+}
